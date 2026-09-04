f0563acd3f73f999bdd208fd8f0e7171e6013000
Jon Eric Tripulca <joneric13tripulca@gmail.com>
2026-09-04 10:21:04 +0800

    feature/home-page

---
+++ resources/js/pages/home/home.jsx
@@ -0,0 +1,25 @@
+import Navbar from '../../components/common/Navbar';
+import Footer from '../../components/common/Footer';
+import Cta from './cta';
+import ExploreTrail from './ExploreTrail';
+import Gallery from './Gallery';
+import Experience from './Experience';
+import Awards from './Awards';
+import News from './News';
+
+export default function Home() {
+    return (
+        <div className="flex min-h-screen flex-col bg-surface">
+            <Navbar />
+            <main className="-mt-13 flex-1 lg:-mt-14">
+                <Cta />
+                <ExploreTrail />
+                <Gallery />
+                <Experience />
+                <Awards />
+                <News />
+            </main>
+            <Footer />
+        </div>
+    );
+}
